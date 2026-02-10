eab2adc58d70dea0d947967c0f9ca707b6b5998a
Michal Bryxí <michal.bryxi@gmail.com>
2026-02-10 16:50:13 +0100

    🧩 add ember-modify-based-class-resource dependency and Vite alias

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,17 @@ import { defineConfig } from 'vite';
 import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
+const nm = `${process.cwd()}/node_modules`;
+
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: /ember-modify-based-class-resource$/,
+        replacement: `${nm}/ember-modify-based-class-resource/dist/index.js`,
+      },
+    ],
+  },
   plugins: [
     classicEmberSupport(),
     ember(), // extra plugins here
