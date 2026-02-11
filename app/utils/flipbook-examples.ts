e702f006bab83914a1f68ec41b03389a95b5deb9
Michal Bryxí <michal.bryxi@gmail.com>
2026-02-11 09:37:52 +0100

    ✂️ Shorten sample DnD custom names

--- app/utils/flipbook-examples.ts
+++ app/utils/flipbook-examples.ts
@@ -1,7 +1,7 @@
 export const FLIPBOOK_EXAMPLE_INPUT = [
-  'https://open.spotify.com/track/4uLU6hMCjMI75M1A2tKUQC,Sir Critsalot Thunderbeard',
-  'https://open.spotify.com/track/0GjEhVFGZW8afUYGChu3Rr,Lady Moonwhisper von Sparklehelm',
-  'https://open.spotify.com/track/1u7kkVrr14iBvrpYnZILJR,Grunkle Glitteraxe the Third',
-  'https://open.spotify.com/track/1H5tvpoApNDxvxDexoaAUo,Hexmaster Wobbleboots of Eldersnack',
-  'https://open.spotify.com/track/7rIovIsXE6kMn629b7kDig,Bard Supreme Twinklefang',
+  'https://open.spotify.com/track/4uLU6hMCjMI75M1A2tKUQC,Rick',
+  'https://open.spotify.com/track/0GjEhVFGZW8afUYGChu3Rr,Miri',
+  'https://open.spotify.com/track/1u7kkVrr14iBvrpYnZILJR,Grum',
+  'https://open.spotify.com/track/1H5tvpoApNDxvxDexoaAUo,Hex',
+  'https://open.spotify.com/track/7rIovIsXE6kMn629b7kDig,Fizz',
 ].join('\n');
